6e8f875fd58cbaa0ff573cb010324ab86f530814
César López Ramírez <cesarlr@gmail.com>
2024-03-04 11:16:04 +0100

    Added state

--- oudur/models/models.py
+++ oudur/models/models.py
@@ -9,6 +9,7 @@ class Egg(models.Model):
 
      harvest_date = fields.Date('Harvest Date', default=fields.Date.today())
      serial_number = fields.Char('Serial Number', size=12, default=lambda self: self._get_sequence())
+     state = fields.Selection([('fresh', 'Fresh'), ('poached', 'Poached'), ('hard', 'Hard')], 'State')
 
      def _get_sequence(self):
         return self.env['ir.sequence'].next_by_code('egg.serial')
